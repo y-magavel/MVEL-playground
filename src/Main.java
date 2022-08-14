f55173962a5b07b554c5904d877a496af24b6d6d
y-magavel <magavel.dev@gmail.com>
2022-08-14 14:38:17 +0900

    Main.Javaからmain.mvelを呼び出して実行するよう実装
    これ以降は基本的にMVELファイルにプログラムを記述して実行するのでMain.Javaはほとんど変更しない予定。

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.*;
+import org.mvel2.MVEL;
+
 public class Main {
+
+    /** ここからプログラムが始まる */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! from Java");
+
+        try {
+            execMvel(); // MVELファイルの実行
+        } catch (IOException e) {
+            // MVELファイルを開く際にエラーが発生した場合
+            System.out.println(e);
+        }
+    }
+
+    /** /usr/src/mvelディレクトリにあるMVELファイルの処理を実行する */
+    public static void execMvel() throws IOException {
+        MVEL.evalFile(new File("mvel/main.mvel"), "UTF-8");
     }
 }
